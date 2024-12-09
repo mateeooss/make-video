25027cf7d3bed61c79e223ecd3f4c9cfbe89d313
mateeooss <matheus_limaalmeida3@hotmail.com>
2024-12-09 00:43:50 -0300

    feat: commit inicial

---
+++ src/test/java/com/makevideo/make_video/MakeVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makevideo.make_video;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeVideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
