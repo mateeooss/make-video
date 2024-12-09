d32d5e300b3d9a3402ff5b907305ccf026deb574
mateeooss <matheus_limaalmeida3@hotmail.com>
2024-12-09 00:43:50 -0300

    feat: commit inicial

---
+++ src/main/java/com/makevideo/make_video/MakeVideoApplication.java
@@ -0,0 +1,13 @@
+package com.makevideo.make_video;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeVideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeVideoApplication.class, args);
+	}
+
+}
